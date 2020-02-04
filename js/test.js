452daba680f2ab863ca56fb68df6811c5701c636
Erick <erick.e.carrasco88@gmail.com>
2020-02-04 09:27:07 -0600

    Actualizaciones de la pagina index.hntml

---
+++ js/test.js
@@ -0,0 +1,15 @@
+x=1;
+function myFunction1(){
+    
+    if(x==0){
+        document.getElementById("myImage").src="images/uselessdata2.png";
+        x=1;
+    }else{
+        document.getElementById("myImage").src="images/uselessdata.png";
+        x=0;
+    }
+    
+}
+function alertBox(){
+    alert("Nani the fuck?");
+}
